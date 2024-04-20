5fef22e1949d7e89d95d5342ddbf825616577200
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-04-20 22:58:24 +0300

    EntityAndDataAccessLayersCompleted

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager 
+    {
+        GenericRepository<Category> repository;
+        public CategoryManager()
+        {
+            repository = new GenericRepository<Category>();
+        }
+
+        public List<Category> GetAll()
+        {
+          return repository.List();
+        }
+
+        public void CategoryAdd(Category p) 
+        { 
+            if(p.CategoryName =="" || p.CategoryName.Length <=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
+            {
+                // error message
+            }
+            else
+            {
+                repository.Insert(p);
+            }        
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context db;
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+             db = new Context();
+            _object = db.Set<T>();
+        }
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            db.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            db.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            db.SaveChanges();
+        }
+    }
+}
