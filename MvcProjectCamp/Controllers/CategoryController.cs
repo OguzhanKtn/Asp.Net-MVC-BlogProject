31d88916f674070c5a8e71d544a3cbd7eb13e3db
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-04-22 18:01:18 +0300

    CategoryController

---
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager manager;
+
+        public CategoryController()
+        {
+            manager = new CategoryManager();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = manager.GetAll();
+            return View(categoryvalues);
+        }
+    }
+}
