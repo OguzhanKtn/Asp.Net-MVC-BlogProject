cfd086ff0753be58c113a8a662e1ded2939da96e
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-05-10 23:04:38 +0300

    ErrorPage

--- MvcProjectCamp/Controllers/MessageController.cs
+++ MvcProjectCamp/Controllers/MessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,7 @@ namespace MvcProjectCamp.Controllers
     public class MessageController : Controller
     {
         MessageManager messageManager;
+        
         public MessageController() 
         { 
             messageManager = new MessageManager(new EfMessageDal());
@@ -34,15 +37,36 @@ namespace MvcProjectCamp.Controllers
             return View(values);
         }
 
+        public ActionResult GetSendboxDetails(int id)
+        {
+            var values = messageManager.GetByID(id);
+            return View(values);
+        }
+
         [HttpGet]
         public ActionResult NewMessage() 
-        { 
+        {
             return View();
         }
 
         [HttpPost]
-        public ActionResult NewMessage(Message message) 
+        public ActionResult NewMessage(Message p) 
         {
+            MessageValidator messageValidator = new MessageValidator();
+            ValidationResult results = messageValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                messageManager.Add(p);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
