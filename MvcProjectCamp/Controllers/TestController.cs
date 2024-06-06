cd58cc0a4c290d54417aa1f98267f1864686d63d
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-06-06 20:25:53 +0300

    AuthorizationControl

---
+++ MvcProjectCamp/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SweetAlert()
+        {
+            return View();
+        }
+    }
+}
